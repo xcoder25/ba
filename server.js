f2edac8de486d2e1ad3c27d0fbebcd4f33e46bf8
JJmoff <jahsfreedman12@gmail.com>
2025-09-05 00:18:09 +0100

    update

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const { spawn } = require("child_process");
 
-const pb = spawn("./pocketbase", ["serve", "--http=0.0.0.0:8090"], {
+const port = process.env.PORT || "8090"; // Railway provides $PORT
+console.log("Starting PocketBase on port", port);
+
+const pb = spawn("./pocketbase", ["serve", `--http=0.0.0.0:${port}`], {
   stdio: "inherit"
 });
 
